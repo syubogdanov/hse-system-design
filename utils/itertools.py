78e43f4c8799e890a6d6ff70596dfef69da323d9
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-11-18 12:17:04 +0300

    Update itertools.py

--- utils/itertools.py
+++ utils/itertools.py
@@ -1,6 +1,6 @@
+from asyncio import FIRST_COMPLETED, Task, create_task, wait
 from collections.abc import AsyncIterable, Callable, Coroutine
 from typing import Any, TypeVar
-
 
 T = TypeVar("T")
 
@@ -10,5 +10,14 @@ async def aiomap(
     iterable: AsyncIterable[T],
     max_concurrent_tasks: int = 1,
 ) -> None:
-    """Применить функцию к итерирумому объекту."""
-    raise NotImplementedError
+    """Конкурентно применить функцию."""
+    tasks: set[Task] = set()
+
+    async for item in iterable:
+        if len(tasks) >= max_concurrent_tasks:
+            _, tasks = await wait(tasks, return_when=FIRST_COMPLETED)
+
+        task = create_task(func(item))
+        tasks.add(task)
+
+    await wait(tasks)
