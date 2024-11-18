c3020aec804bd60a43157b5f32ab02747e0f07a7
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-11-18 19:19:23 +0300

    feat: exclude status

--- src/domain/entities/order.py
+++ src/domain/entities/order.py
@@ -1,23 +1,9 @@
-from enum import StrEnum, auto
-from typing import Self
 from uuid import UUID
 
 from pydantic import BaseModel
-
-
-class OrderStatus(StrEnum):
-    """Статус заказа."""
-
-    CANCELED = auto()
-    FINISHED = auto()
-
-    def is_final(self: Self) -> bool:
-        """Проверить, что статус финальный."""
-        return self in {OrderStatus.CANCELED, OrderStatus.FINISHED}
 
 
 class Order(BaseModel):
     """Сущность заказа."""
 
     id: UUID
-    status: OrderStatus
